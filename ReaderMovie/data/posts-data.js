62a04b9bfb0da8f9eb4fbb94774458db18795f5a
964269669 <964269669@qq.com>
2017-07-17 17:55:42 +0800

    分离模板数据

---
+++ ReaderMovie/data/posts-data.js
@@ -0,0 +1,52 @@
+var local_database=[
+      {
+        date:"Sep 18 2016",
+        title:"正是虾肥蟹壮时",
+        imgSrc:"/images/post/crab.png",
+        avatar:"/images/avatar/1.png",
+        content:"菊黄蟹正肥 品尝秋之味。蟹，节肢动物甲壳类，全身有甲壳，头胸甲发达，腹部退化，俗称“脐”，雄的尖脐，雌的团脐。有脚五对，第一对脚成钳状，叫螯，用来捕食和御敌，横着爬行。",
+        reading:"112",
+        collection:"96"
+      },
+      {
+        date:"Nov 25 2016",
+        title:"比利·林恩的中场故事",
+        imgSrc:"/images/post/bl.png",
+        avatar:"/images/avatar/2.png",
+        content:"李安是一位绝不会重复自己的导演，本片将极富原创性李安众所瞩目的新片《比利·林恩的中场故事》",
+        reading:"110",
+        collection:"75"
+      },
+      {
+        //按住alt + shift + F 可以格式化代码样式
+        title: "当我们在谈论经济学时，我们在谈论什么?",
+        content: "引言在我跟学生课后交流时，以及我在知乎上阅读有关“经济”问题的论题时，经常会遇到这样的情况：...",
+        imgSrc: "/images/post/sls.jpg",
+        reading: 62,
+        collection: 92,
+        date: "Nov 12 2016",
+        avatar: "/images/avatar/3.png",
+       },
+       {
+        title: "微信·小程序开发工具安装指南",
+        content: "这两天闲来无事，也安装了 “微信折叠”的开发工具来玩一下。以下是一些小道消息及使用体验，过两天我会写一篇文章以开发者的角度来详细评价微信小程序",
+        imgSrc: "/images/post/xiaolong.jpg",
+        reading: 102,
+        collection: 92,
+        date: "Nov 20 2016",
+        avatar: "/images/avatar/5.png"
+      },
+      {
+        title: "从视觉到触觉 这款VR手套能给你真实触感",
+        content: "8月29日消息，据国外媒体VentureBeat报道，一家名为Dexta Robotics的公司最近发布了一款有望变革虚拟现实手部追踪与交互方式的新产品",
+        imgSrc: "/images/post/vr.png",
+        reading: 102,
+        collection: 26,
+        date: "Nov 20 2016",
+        avatar: "../../../images/avatar/3.png"
+       }
+    ]
+
+    module.exports={
+      postList:local_database
+    }
--- ReaderMovie/pages/posts/post.js
+++ ReaderMovie/pages/posts/post.js
@@ -1,89 +1,24 @@
-Page({
+var postsData=require('../../data/posts-data.js')
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+Page({
+  data:{
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var posts_content=[
-      {
-        date:"Sep 18 2016",
-        title:"正是虾肥蟹壮时",
-        post_img:"/images/post/crab.png",
-        content:"菊黄蟹正肥 品尝秋之味。蟹，节肢动物甲壳类，全身有甲壳，头胸甲发达，腹部退化，俗称“脐”，雄的尖脐，雌的团脐。有脚五对，第一对脚成钳状，叫螯，用来捕食和御敌，横着爬行。",
-        view_num:"112",
-        collect_num:"96",
-        author_img:"/images/avatar/1.png"
-      },
-      {
-        date:"Nov 25 2016",
-        title:"比利·林恩的中场故事",
-        post_img:"/images/post/bl.png",
-        content:"李安是一位绝不会重复自己的导演，本片将极富原创性李安众所瞩目的新片《比利·林恩的中场故事》",
-        view_num:"110",
-        collect_num:"75",
-        author_img:"/images/avatar/2.png"
-      }
-    ]
+
+    // this.data.postList=postsData.postList;
+    // console.log(this.data.postList);
     //相当于把json中的数据放到了data数据中
     this.setData({
-      posts_key:posts_content
+      posts_key:postsData.postList
     })
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
   }
+
+  
 })
 
 
