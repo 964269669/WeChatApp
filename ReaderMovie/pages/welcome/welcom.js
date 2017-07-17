69991b7375e37900348edc90f75b4ecf55277f26
964269669 <964269669@qq.com>
2017-07-17 15:53:59 +0800

    起始页页面跳转

--- ReaderMovie/pages/welcome/welcom.js
+++ ReaderMovie/pages/welcome/welcom.js
@@ -8,39 +8,51 @@ Page({
   
   },
 
+  onContainerTap:function(){
+   /* //导航路由  页面跳转  
+   从父跳转到子，可以返回，父子级深度最多五级,调用了生命周期的onHide方法，也就是父页面被隐藏
+    wx.navigateTo({
+      url:"../posts/post"
+    });*/
+    // 页面之间平行跳转 不能返回，调用了生命周期的unLoad方法
+    wx.redirectTo({
+      url:"../posts/post"
+    })
+
+  },
+
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+    
   },
 
   /**
