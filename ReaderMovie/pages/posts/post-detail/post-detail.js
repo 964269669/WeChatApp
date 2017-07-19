471b47ca188e5a37127bb733f19047e253d0679e
964269669 <964269669@qq.com>
2017-07-19 17:48:32 +0800

    异步与同步

--- ReaderMovie/pages/posts/post-detail/post-detail.js
+++ ReaderMovie/pages/posts/post-detail/post-detail.js
@@ -14,12 +14,6 @@ Page({
       postData_key:postData
     });
 
-    // var postsCollected={
-    //   1:true,
-    //   2:false,
-    //   3:true
-    // }
-    
     var postsCollected=wx.getStorageSync("posts_collected")
     if(postsCollected){
       var postCollected=postsCollected[postId];
@@ -34,17 +28,88 @@ Page({
     }
     
   },
+  //收藏功能
   onCollectionTap:function(event){
+    this.getPostsCollectedSyc();
+    // this.getPostsCollectedAsy();
+  },
+  //异步方法
+  getPostsCollectedAsy:function(){
+    var that=this;
+    wx.getStorage({
+      key:"posts_collected",
+      success:function(res){
+        var postsCollected=res.data;
+        var postCollected=postsCollected[that.data.currentPostId];
+        //收藏变成未收藏  未收藏变成收藏
+        postCollected=!postCollected;
+        postsCollected[that.data.currentPostId]=postCollected;
+        that.showToast(postsCollected,postCollected);
+      }
+    })
+  },
+
+  //同步方法
+  getPostsCollectedSyc:function(){
     var postsCollected=wx.getStorageSync("posts_collected");
     var postCollected=postsCollected[this.data.currentPostId];
     //收藏变成未收藏  未收藏变成收藏
     postCollected=!postCollected;
     postsCollected[this.data.currentPostId]=postCollected;
+    this.showToast(postsCollected,postCollected);
+  },
+
+  showModal:function(postsCollected,postCollected){
+    var that=this;
+    wx.showModal({
+      title:"收藏",
+      content:postCollected ? "收藏该文章？":"取消收藏该文章？",
+      showCancel:"true",
+      cancelText:"取消",
+      cancelColor:"#333",
+      confirmText:"确认",
+      confirmColor:"#405f80",
+      success:function(res){
+        if(res.confirm){
+          //更新文章是否缓存
+          wx.setStorageSync("posts_collected",postsCollected);
+          //更新数据绑定变量，从而实现切换图片
+          that.setData({
+            collected:postCollected
+          });
+        }
+      }
+    })
+  },
+  showToast:function(postsCollected,postCollected){
     //更新文章是否缓存
     wx.setStorageSync("posts_collected",postsCollected);
     //更新数据绑定变量，从而实现切换图片
     this.setData({
       collected:postCollected
+    });
+
+    //收藏文字提示
+    wx.showToast({
+      title:postCollected ? "收藏成功" : "取消收藏",
+      duration:1000,
+      icon:"success"
+    })
+  },
+  //分享功能
+  onShareTap:function(){
+    var itemList=[
+        "分享给微信好友","分享到朋友圈","分享给QQ好友","分享到QQ空间"
+      ];
+    wx.showActionSheet({
+      itemList:itemList,
+      itemColor:"#f00",
+      success:function(res){
+        // wx.showModal({
+        //   title:"用户"+itemList[res.tapIndex],
+        //   content:"用户是否取消？"+res.cancel
+        // })
+      }
     })
   }
   
