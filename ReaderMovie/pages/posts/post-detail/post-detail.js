5b75932798cf049b77a1273c66d7e5fb2592aaa0
964269669 <964269669@qq.com>
2017-07-19 15:39:11 +0800

    缓存模仿收藏功能

--- ReaderMovie/pages/posts/post-detail/post-detail.js
+++ ReaderMovie/pages/posts/post-detail/post-detail.js
@@ -5,13 +5,49 @@ Page({
   },
   onLoad:function(option){
     var postId=option.id;
+    //中转postId
+    this.data.currentPostId=postId;
+    //获取列表数据
     var postData=postsData.postList[postId];
-    console.log(postData);
     // this.data.postData=postData;
     this.setData({
       postData_key:postData
+    });
+
+    // var postsCollected={
+    //   1:true,
+    //   2:false,
+    //   3:true
+    // }
+    
+    var postsCollected=wx.getStorageSync("posts_collected")
+    if(postsCollected){
+      var postCollected=postsCollected[postId];
+      //将collected和布尔值postCollected绑定
+      this.setData({
+        collected:postCollected
+      })
+    }else{
+      var postsCollected={};
+      postsCollected[postId]=false;
+      wx.setStorageSync("posts_collected",postsCollected);
+    }
+    
+  },
+  onCollectionTap:function(event){
+    var postsCollected=wx.getStorageSync("posts_collected");
+    var postCollected=postsCollected[this.data.currentPostId];
+    //收藏变成未收藏  未收藏变成收藏
+    postCollected=!postCollected;
+    postsCollected[this.data.currentPostId]=postCollected;
+    //更新文章是否缓存
+    wx.setStorageSync("posts_collected",postsCollected);
+    //更新数据绑定变量，从而实现切换图片
+    this.setData({
+      collected:postCollected
     })
   }
+  
 })
 
 
